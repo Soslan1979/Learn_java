baeb2580715e66dc4b4d35f0ff1272fbab496437
geekbrains <info@geekbrains.ru>
2022-02-15 16:52:49 +0300

    test2 commit

---
+++ src/AveCoder/lesson_1.java
@@ -0,0 +1,19 @@
+package AveCoder;
+
+
+class Book {
+    String bookName;
+    Book() {
+        Book b1 = new Book();
+        b1.bookName = "Java OCA";
+    }
+}
+
+class TestBook {
+    public static void main(String[] args) {
+        Book b2 = new Book();
+        b2.bookName = "Java OCP";
+        System.out.println(b2.bookName);
+    }
+}
+
